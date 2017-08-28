40eba238c8a43bda07b76b7ff1b195f600ddb435
Utsav Sethi <utsavsethi@gmail.com>
2017-08-28 14:22:21 -0500

    fix setup tests

--- src/tic_tac_toe/console_game_setup.clj
+++ src/tic_tac_toe/console_game_setup.clj
@@ -5,26 +5,33 @@
 
 
 ;TODO remove hard-coded choices 
+;TODO rename game-type to something else
 (defn get-game-type [options]
   (console-ui/print-menu options)
   (let [choice (read-string (console-ui/get-user-input))] 
     (case choice 
-      1 :human-vs-human
-      2 :human-vs-computer 
-      3 :human-vs-hard-computer 
+      1 [:human :human]
+      2 [:human :computer] 
+      3 [:human :hard-computer] 
       (do (console-ui/print-message "Bad Option") (recur options)))))
 
-(defn- construct-game [board-size players]
-  (let [[current-player opponent-player] players
-        board (board/new-board board-size)] 
-    {:board board 
-     :current-player current-player 
-     :opponent-player opponent-player}))
+(defn setup-players [game-type]
+  (let [[main-player-type opponent-player-type] game-type
+        main-player (player-setup/initialize-player main-player-type)
+        opponent-player (player-setup/initialize-player opponent-player-type (:marker main-player))]
+    [main-player opponent-player]))
 
-;TODO move high-level player initialization logic into console-game-setup, so level of abstraction is consistent
+(defn- make-game [players board]
+  (let [[main-player opponent-player] players] 
+    {:current-player main-player 
+     :opponent-player opponent-player
+     :board board }))
+
+;TODO rename new-game/make-game
 (defn new-game [configuration]
   (let [{:keys [board-size options]} configuration
-        game-type (get-game-type options)]
-    (->> (player-setup/initialize-players game-type)
-         (construct-game board-size))))
+        game-type (get-game-type options)
+        players (setup-players game-type)
+        board (board/new-board board-size)]
+    (make-game players board)))
 
--- src/tic_tac_toe/player_setup.clj
+++ src/tic_tac_toe/player_setup.clj
@@ -2,21 +2,17 @@
   (:require [tic-tac-toe.console-ui :as console-ui]
             [tic-tac-toe.validation :as validation]))
 
-;TODO clean up
 (def default-computer-markers [:O :X])
 
 (defmulti get-marker (fn [player-type taken-marker] player-type))
-
-(defn- invalid-human-marker [error-str taken-marker]
-  (console-ui/print-message error-str)
-  (get-marker :human taken-marker))
 
 (defmethod get-marker :human [player-type taken-marker]
   (console-ui/print-marker-prompt taken-marker)
   (let [marker (console-ui/get-user-input)]
     (if-let [error-str (:error (validation/marker marker taken-marker))] 
-      (invalid-human-marker error-str taken-marker) 
+      (do (console-ui/print-message error-str) (recur player-type taken-marker)) 
       (keyword marker))))
+
 
 (defn- default-marker [taken-marker]
   (if (= taken-marker (first default-computer-markers))
@@ -28,19 +24,9 @@
     (console-ui/print-computer-marker marker)
     marker))
 
-(defn- create-player 
-  ([player-type] (create-player player-type nil))
+(defn initialize-player 
+  ([player-type] (initialize-player player-type nil))
   ([player-type taken-marker] 
    (let [marker (get-marker player-type taken-marker)] 
      {:type player-type :marker marker})))
 
-(defn- create-opponent [game-type human-marker]
-  (case game-type
-      :human-vs-human (create-player :human human-marker)
-      :human-vs-computer (create-player :computer human-marker)
-      :human-vs-hard-computer (create-player :hard-computer human-marker)))
-
-(defn initialize-players [game-type]
-  (let [human-player (create-player :human)
-        opponent-player (create-opponent game-type (:marker human-player))] 
-    [human-player opponent-player]))
