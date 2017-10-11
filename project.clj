ea621184b934574ebf708c79af13d9592bc85499
Utsav Sethi <utsavsethi@gmail.com>
2017-10-11 12:01:16 -0500

    fix dependencies

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main tic-tac-toe.core-console
-  :dependencies [[org.clojure/clojure "1.8.0"]] 
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.5.0"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [ring/ring-json "0.4.0"]] 
   :profiles {:dev {:dependencies [[speclj "3.3.2"]]}
-             :web {:dependencies [[ring/ring-core "1.5.0"]
-                                  [ring/ring-jetty-adapter "1.5.0"]
-                                  [ring/ring-json "0.4.0"]]
-                   :main tic-tac-toe.core-web}}
+             :web {:main tic-tac-toe.core-web}}
   :plugins [[speclj "3.3.2"]]
   :test-paths ["spec"])
